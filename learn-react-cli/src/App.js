c67aaaa6422b6bff3ba66998934e0bb68932e21f
Human Town <ty.1997@foxmail.com>
2022-03-17 23:32:30 +0800

    路由跑通就算成功

--- learn-react-cli/src/App.js
+++ learn-react-cli/src/App.js
@@ -1,14 +1,28 @@
-
 import './App.css';
-import React,{Component} from "react";
-import Hello from "./components/Hello";
+import React, {Component} from "react";
+import {Link, BrowserRouter, Route, Routes} from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+
 
 export default class App extends Component {
+
     render() {
+
         return (
             <div>
-                <h1>hello react</h1>
-                <Hello name='child-in-props'/>
+                <BrowserRouter>
+                    <h1>Learn React CLI</h1>
+                    <Link to="/about">About</Link><br/>
+                    <Link to="/home">about</Link>
+                    <div>
+                        <Routes>
+                            <Route path="/about" element={<About/>}/>
+                            <Route path="/home" element={<Home/>}/>
+                        </Routes>
+
+                    </div>
+                </BrowserRouter>
             </div>
         )
     }
