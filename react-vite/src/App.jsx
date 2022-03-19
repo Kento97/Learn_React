d7124863cdec28571fb3461f096420bc1bcd6185
Human Town <ty.1997@foxmail.com>
2022-03-19 12:06:58 +0800

    useNavigate 可以传递复杂对象

--- react-vite/src/App.jsx
+++ react-vite/src/App.jsx
@@ -3,15 +3,21 @@ import './App.css'
 import Home from "./pages/home.jsx";
 import About from "./pages/about.jsx";
 import Router from "./router/index.jsx";
-import {Link, Outlet, useLocation} from "react-router-dom";
+import {Link, Outlet, useLocation, useNavigate} from "react-router-dom";
 import Index from "../src/pages/router-demo/Index";
-
 
 
 function App() {
     const [count, setCount] = useState(0)
-    const location = useLocation()
-    console.log(location.pathname,"location")
+    // const location = useLocation();
+    const navigate = useNavigate();
+    const goDetail = () => {
+        navigate(`/detail`, {
+            state: {
+                name: "张三"
+            }
+        })
+    }
     return (
         <div className="App"
              style={{fontSize: "40px", fontFamily: "JetBrains Mono", display: "flex", flexDirection: "column"}}>
@@ -19,12 +25,15 @@ function App() {
             <Link to="/detail">detail</Link>
             <Link to="/list">list</Link>
             <Link to="/error">404</Link>
+            <button onClick={goDetail}>跳转详情页
+            </button>
             <div style={{
                 borderRadius: "10px",
                 height: "100px",
                 lineHeight: "100px",
                 textAlign: "center",
-                background: "darkgrey"
+                background: "darkgrey",
+                marginTop: "40px"
             }}>
                 <Outlet/>
             </div>
--- react-vite/src/pages/router-demo/Detail.jsx
+++ react-vite/src/pages/router-demo/Detail.jsx
@@ -1,11 +1,14 @@
 import {useState} from 'react';
+import {useLocation, useParams, useSearchParams} from "react-router-dom";
 
 
-const Detail=() => {
-
+const Detail = () => {
+    const location = useLocation()
+    console.log('state:',location.state)
+    const state = location.state
     return (
         <>
-            <div>详情</div>
+            <div>详情-{state && state.name || ""}</div>
         </>
     );
 
