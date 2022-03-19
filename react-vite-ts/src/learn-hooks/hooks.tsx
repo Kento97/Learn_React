b4649ed94774cea96eb2839b677da9b8e1e799ce
Human Town <ty.1997@foxmail.com>
2022-03-19 23:16:59 +0800

    react pending...

--- react-vite-ts/src/learn-hooks/hooks.tsx
+++ react-vite-ts/src/learn-hooks/hooks.tsx
@@ -1,32 +1,34 @@
-import {useState, useEffect, useRef} from 'react';
+import {
+    useState,
+    useEffect,
+    useRef,
+    useContext,
+    createContext,
+} from 'react';
 
-type Props = {
-    [key: string]: number
+
+const Context = createContext(0)
+const B = () => {
+    const count = useContext(Context);
+    return <div>{count}</div>
 }
 
-function getDefaultValue() {
-    for (let i = 0; i < 100000; i++) {
-        if (i === 9999) return i;
-    }
-    return 0;
-}
 
-const Counter = (props: Props) => {
-    const [count, setCount] = useState(() => props.count);
-    const addCounter = () => setCount(count + 1)
-    return (
-        <div>
-            <button onClick={addCounter}>{count}</button>
-        </div>
-    )
+const A = () => {
+    return <div>
+        A
+        <B/>
+    </div>
 }
 
 const Hooks = () => {
-
+    const [count, setCount] = useState(0)
     return (
         <div>
-            <Counter count={10}/>
-            <Counter count={getDefaultValue()}/>
+            <Context.Provider value={count}>
+                <A/>
+            </Context.Provider>
+
         </div>
     )
 }
