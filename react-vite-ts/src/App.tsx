08e7e5cc99fb28ddb0858051d7c233c5ee3f9fdf
Human Town <ty.1997@foxmail.com>
2022-04-14 21:56:32 +0800

    Updating Arrays in State-useState文档部分翻译

--- react-vite-ts/src/App.tsx
+++ react-vite-ts/src/App.tsx
@@ -1,16 +1,13 @@
 import {useState} from 'react'
 import './App.css'
 import {useNavigate, Routes} from "react-router-dom";
-import MutateArray from "@/learn-hooks/MutateArray";
-import MutateObject from "@/learn-hooks/MutateObject";
 
 
 function App() {
 
     return (
         <div className="App">
-            <MutateArray/>
-            <MutateObject/>
+
         </div>
     )
 }
